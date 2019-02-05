3bdec00a5cbb35c3a7b087dac35eb25cbe7306ec
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-05 12:14:42 +0000

    created a test to accept a firstname in the firstname field

--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -7,12 +7,18 @@ describe 'Testing the demo qa automation form' do
     @driver = QaToolsForm.new
     @driver.visit_practice_form
     @url = 'https://www.toolsqa.com/automation-practice-form'
+    @name = Faker::HarryPotter.character
   end
 
   context 'Testing the positive paths for the form' do
 
     it 'should land on the registration page' do 
       expect(@driver.current_url).to eq @url
+    end
+
+    it 'should accept a firstname' do
+     @driver.input_firstname_field('Kirpal')
+     expect(@driver.input_firstname_field_value).to eq 'Kirpal'
     end
 
   end
