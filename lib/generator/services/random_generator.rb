4d68bbd05339320d601f5f0ef82a66393a6ee314
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-05 14:58:55 +0000

    created methods to get the first and last name in the random_generator.rb file using Faker

--- lib/generator/services/random_generator.rb
+++ lib/generator/services/random_generator.rb
@@ -1,8 +1,17 @@
-require 'Faker'
+require 'faker'
 
 class RandomFormData
 
-  
+  def first_name
+    Faker::Name.first_name
+  end
+
+  def last_name
+    Faker::Name.last_name
+  end
+
+
+
 
 
 end
